38ff57d42cd9c94c352b16d9cbed33a1ad514d0f
irfan-2000 <irfan201819@gmail.com>
2026-01-14 19:47:43 +0530

    irfan

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
 
     // baseUrl: 'https://localhost:7203/'
 // baseUrl: 'https://admin.api.race-elearn.com:444/'
-   baseUrl: 'http://admin.api.drbagchisclasses.com/',
+   baseUrl: 'https://admin.api.drbagchisclasses.com/',
     userurl:'https://api.drbagchisclasses.com/'
    // baseUrl:'https://api.race-elearn.com/'
   };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment =
   production: false,
   // baseUrl: 'https://localhost:7091/',
    //userurl:'http://localhost:8080/'
-   baseUrl: 'http://admin.api.drbagchisclasses.com/',
+   baseUrl: 'https://admin.api.drbagchisclasses.com/',
     userurl:'https://api.drbagchisclasses.com/'
 
   };
